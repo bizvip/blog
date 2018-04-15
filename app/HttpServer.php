e38abda61a43e60f62d946faeae9f39682abb809
lee4github <axiba188@gmail.com>
2018-04-16 02:23:08 +0800

    index.html

---
+++ app/HttpServer.php
@@ -0,0 +1,32 @@
+<?php declare(strict_types=1);
+
+use Swoole\Http\Server;
+
+final class HttpServer {
+    
+    public static $httpServer = NULL;
+    
+    public function __construct() {
+        self::$httpServer = new Server('0.0.0.0', 9002);
+        self::$httpServer->set([
+            'reactor_num'     => 4,
+            'worker_num'      => 4,
+            'max_conn'        => 55555,
+            'daemonize'       => FALSE,
+            'http_parse_post' => FALSE,
+        ]);
+        self::$httpServer->on('Request', [$this, 'onRequest']);
+        self::$httpServer->on('WorkerStart', [$this, 'onWorkerStart']);
+        self::$httpServer->on('WorkerStop', [$this, 'onWorkerStop']);
+        self::$httpServer->start();
+    }
+    
+    public function onRequest(Request $request, Response $response) {
+    }
+    
+    public function onWorkerStart() {
+    }
+    
+    public function onWorkerStop() {
+    }
+}
